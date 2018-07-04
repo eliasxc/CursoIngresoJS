ed202ecf26714827ce56b58958cf07b2cb6677d4
eliasxc <40763820+eliasxc@users.noreply.github.com>
2018-07-04 11:59:35 -0300

    recuperatorio ejerc 1y2

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/dos.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/dos.js
@@ -1,4 +1,14 @@
 function mostrar()
 {
   
+	var nombre ;
+
+	nombre = document.getElementById("elNombre").value;
+
+	var localidad;
+
+	localidad = document.getElementById("laLocalidad").value;
+
+	alert("Su nombre es "+nombre+" y vive en la localidad de "+localidad);
+
 }
--- 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
@@ -1,5 +1,24 @@
 
 function mostrar()
 {
+var base , altura;
+
+base = prompt( "ingrese base: ");
+
+base = parseint(base);
+
+altura = prompt("ingrese altura: ");
+
+altura = parseint(altura);
+
+var perimetro , superficie ;
+
+perimetro = base * 3 ;
+
+supertficie = (base*altura) / 2;
+
+alert ("el primetro sera: "+perimetro+"y la superficie: "+ superficie);
+
+
 
 }
