ab3033c1b10885f09d3df353b3d8b45c094f2b12
eliasxc <40763820+eliasxc@users.noreply.github.com>
2018-07-04 12:22:17 -0300

    recu ejerc 3

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/tres.js
@@ -1,4 +1,23 @@
 function mostrar()
 {
+     var precio , porcentaje ;
+
+    precio = prompt("ingrese precio: ");
+
+    porcentaje = prompt("ingrese descuento: " , "%");
+
+    descuento = porcentaje/100 ;
+
+    Ddescuento = precio * descuento ;
+
+    Pdescuento = precio - Ddescuento ;
+
+    IVA = 21 / precio ;
+
+    alert ("el precio del descuento sera")
+
+
+
+
 
 }
