082538184a719fe4a0415a8625ab7c60107f270d
eliasxc <40763820+eliasxc@users.noreply.github.com>
2018-07-23 12:21:00 -0300

    Add files via upload

--- 3-InstruccionSwitch/jsInstruccionSwitch(1).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(1).js
@@ -1,9 +1,35 @@
 function mostrar()
 {
-//tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
+     //tomo el mes 
+     var mesDelAño = document.getElementById('mes').value;
 
+   console.log(mesDelAño);
 
+    switch(mesDelAño)
 
+    {
+    	case "Enero" :
+    	{
+    	    alert("que comienzes bien el año");
+    	    break ;
+        }
 
+        case "Marzo" :
+        {
+        	alert("a clases!!");
+        	break ;
+        }
+
+        case "Julio" :
+        {
+        	alert("se vienen las vacaciones");
+        	break ;
+        }
+
+        case "Diciembre" :
+        {
+        	alert("felices fiestas!!!");
+        	break ;
+        }
+    }
 }//FIN DE LA FUNCIÓN
