d9b3482fecb53a3c3c98c5ade96a536eb51d4e01
eliasxc <40763820+eliasxc@users.noreply.github.com>
2018-07-31 18:55:21 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,21 @@
 function mostrar()
 {
-	//Genero el número RANDOM entre 1 y 10 
-	
+//Genero el número RANDOM entre 1 y 10 
+
+var notaRandom ;
+
+notaRandom = parseInt(Math.floor(Math.random()*(10)+1)) ;
+
+if (notaRandom >= 9 ) {
+
+	alert("EXELENTE") ;
+}
+else if (notaRandom >= 4) {
+	alert("APROBO") ;
+}
+else {
+	alert("Vamos, la proxima se puede !!") ;
+}
+
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (9).js
+++ 2-InstruccionIf/jsInstruccionIF (9).js
@@ -2,10 +2,13 @@ function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
 
-	var RANDOM;
+var random ;
 
-	RANDOM = <=10 , >1 ;
+random = Math.floor(Math.random()*(10)+1) ;
 
-	alert (RANDOM)
+random = parseInt(random) ;
+	
+alert (random);
+
 
 }//FIN DE LA FUNCIÓN
