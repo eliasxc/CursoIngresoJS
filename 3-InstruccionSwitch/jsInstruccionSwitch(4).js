7bbc2b86a42ccd5324cd6da5a651dd4914b5f402
eliasxc <40763820+eliasxc@users.noreply.github.com>
2018-07-31 18:56:18 -0300

    Add files via upload

--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -1,11 +1,24 @@
 function mostrar()
 {
 //tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
+var mesDelAño ;
+mesDelAño = document.getElementById('mes').value;
 
 //alert (mesDelAño);
-	
-	
+		
+	switch(mesDelAño) {
+		case "Febrero" :
+			alert("Este mes tiene 29 dias....") ;
+				break ;
+		case "Abril" :
+		case "Junio" :
+		case "Septiembre" :
+		case "Noviembre" :
+			alert("Este mes tiene 30 dias.....") ;
+				break ;
+		default :
+			alert("Este mes tiene 31 dias.....") ;
+	}
 
 
 
